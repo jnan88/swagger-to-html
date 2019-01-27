f59a95788372693deb697e5d955056bdab453a56
qizai <jnan77@qq.com>
2019-01-27 16:03:16 +0800

    增加运行说明

--- src/main/java/xyz/nesting/swaggertohtml/Application.java
+++ src/main/java/xyz/nesting/swaggertohtml/Application.java
@@ -19,6 +19,16 @@ import xyz.nesting.swaggertohtml.util.SwaggerToHtmlUtil;
 public class Application {
 
 	public static void main(String[] args) throws Exception {
+		if (null == args || args.length < 1) {
+			System.out.println(
+					"---------------------------------------------------------------------------------------------------------------------");
+			System.out.println("使用方式：java -jar swagger-to-html.jar [此处是你swaggerAPI的json内容访问地址]");
+			System.out.println("例如：java -jar swagger-to-html.jar \"http://localhost:8080/v2/api-docs\"");
+			System.out.println("运行结果在当前目录，文件名为apidoc.html");
+			System.out.println(
+					"---------------------------------------------------------------------------------------------------------------------");
+			return;
+		}
 		String dir = System.getProperty("user.dir");
 		System.out.println("运行目录为 :" + dir);
 		SwaggerToHtmlUtil.createAdocFile(args[0], dir);
